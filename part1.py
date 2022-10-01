d3452984e7f9d487344d0b09172ba14787b3c8c9
DKFrits <christiaan.gal36@gmail.com>
2022-10-01 16:25:23 +0200

    part1

---
+++ part1.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+
+inputs = [1, 2, 3]
+weights = [0.2, 0.8, -0.5]
+bias = 2
+
+# output = inputs * weights + bias
+output = 0
+for i in range(len(inputs)):
+    output += inputs[i] * weights[i]
+output += bias
+
+print(output)
